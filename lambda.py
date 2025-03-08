28fc4c521059190385ac8be0b1b55b2c9eccb6f2
Vensya Motuti <143604612+vnsya123@users.noreply.github.com>
2025-03-08 15:15:51 +0700

    Add files via upload

---
+++ lambda.py
@@ -0,0 +1,12 @@
+# lambda fungsion 
+
+def kuadrat(angka):
+    return angka**2
+print(f"hasil fungsi kuadrat = {kuadrat(3)}")
+
+
+#contoh mengunakan lambda 
+# output = lambda argument : expression
+
+kuadrat = lambda angka:angka**2
+print(kuadrat(3))
